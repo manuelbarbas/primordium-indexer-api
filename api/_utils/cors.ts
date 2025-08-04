e289c98eb9f48dc22ad866bc562dbfe644703398
manuelbarbas <manuel@skalelabs.com>
2025-08-04 18:55:43 +0100

    cors fix

--- api/_utils/cors.ts
+++ api/_utils/cors.ts
@@ -5,11 +5,12 @@ export function setCorsHeaders(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-api-key');
-  res.setHeader('Access-Control-Max-Age', '86400');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
 
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
-    res.status(200).end();
+    res.status(204).end();
     return true;
   }
 
--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,18 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { setCorsHeaders } from './_utils/cors';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+
+  if (setCorsHeaders(req, res)) {
+    return; // Preflight request handled
+  }
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   return res.json({
-    message: 'emit Herld(); ',
-  })
+    message: "emit Herld(); ",
+  });
 }
